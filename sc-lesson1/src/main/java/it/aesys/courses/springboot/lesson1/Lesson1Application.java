3da075482c449851fd96b58eb1c9c4fb070ad77d
vizzarro <43704449+vizzarro@users.noreply.github.com>
2022-04-08 12:44:34 +0200

    Add Lesson 1 Project fix

--- sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/Lesson1Application.java
+++ sc-lesson1/src/main/java/it/aesys/courses/springboot/lesson1/Lesson1Application.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
