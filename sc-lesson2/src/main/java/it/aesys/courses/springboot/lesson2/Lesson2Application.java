b4cec857f13b03311a35fc15113c77178ecb17a3
vizzarro <43704449+vizzarro@users.noreply.github.com>
2022-04-08 12:46:40 +0200

    release

---
+++ sc-lesson2/src/main/java/it/aesys/courses/springboot/lesson2/Lesson2Application.java
@@ -0,0 +1,50 @@
+package it.aesys.courses.springboot.lesson1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping(path = "/")
+@SpringBootApplication
+public class Lesson2Application {
+
+	private static final Logger logger = LoggerFactory.getLogger(Lesson1Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson1Application.class, args);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String lesson() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("<H1>SPRING BOOT LESSON 1</H!>");
+		builder.append("<H2>alessandro.vizzarro@aesys.tech</H2>");
+		return builder.toString();
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			logger.info("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				logger.info(beanName);
+			}
+		};
+	}
+
+
+
+}
---
+++ sc-lesson2/src/test/java/it/aesys/courses/springboot/lesson2/Lesson2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aesys.courses.springboot.schelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
