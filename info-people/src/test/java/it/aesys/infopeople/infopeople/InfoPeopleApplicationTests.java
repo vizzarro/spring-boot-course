fc7c3c8a0e22c8fca94c942e469d17596596518c
vizzarro <43704449+vizzarro@users.noreply.github.com>
2022-04-21 15:35:39 +0200

    first release

---
+++ info-people/src/test/java/it/aesys/infopeople/infopeople/InfoPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aesys.infopeople.infopeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
