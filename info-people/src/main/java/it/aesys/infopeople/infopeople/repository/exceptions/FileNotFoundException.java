079a3f89f8a6219404614d923659855a4c767d01
Lucy <lucia.dalfonso@aesys.tech>
2022-04-21 17:31:50 +0200

    Repository part implementated

---
+++ info-people/src/main/java/it/aesys/infopeople/infopeople/repository/exceptions/FileNotFoundException.java
@@ -0,0 +1,26 @@
+package it.aesys.infopeople.infopeople.repository.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class FileNotFoundException extends RuntimeException{
+    public FileNotFoundException() {
+    }
+
+    public FileNotFoundException(String message) {
+        super(message);
+    }
+
+    public FileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public FileNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
