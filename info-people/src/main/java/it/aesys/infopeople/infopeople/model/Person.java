f21a89d62a088ce7372e8f72cf2a3eb6dc4ebbf8
Giammarco <giammarco.cataldi@aesys.tech>
2022-04-21 15:53:50 +0200

    add model

---
+++ info-people/src/main/java/it/aesys/infopeople/infopeople/model/Person.java
@@ -0,0 +1,60 @@
+package it.aesys.infopeople.infopeople.model;
+
+import java.util.Date;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private Date birthday;
+    private String taxCode;
+    private Address address;
+
+    public Person(String name, String surname, Date birthday, String taxCode, Address address) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.taxCode = taxCode;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getTaxCode() {
+        return taxCode;
+    }
+
+    public void setTaxCode(String taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
