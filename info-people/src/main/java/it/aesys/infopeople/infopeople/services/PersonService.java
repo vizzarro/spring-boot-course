4bce35fe5f308e82e41873c43642ab858600ee83
Davide <davide.demilio@aesys.tech>
2022-04-22 13:01:42 +0200

    fix import service

--- info-people/src/main/java/it/aesys/infopeople/infopeople/services/PersonService.java
+++ info-people/src/main/java/it/aesys/infopeople/infopeople/services/PersonService.java
@@ -1,11 +1,8 @@
 package it.aesys.infopeople.infopeople.services;
 
 import it.aesys.infopeople.infopeople.dtos.PersonDto;
-import it.aesys.infopeople.infopeople.model.Person;
 import it.aesys.infopeople.infopeople.repository.exceptions.DaoException;
 import it.aesys.infopeople.infopeople.services.exceptions.ServiceException;
-
-import java.util.List;
 
 public interface PersonService {
 
