3220390d4f2571ad41d90dda9f94a20bbe5d2db6
Andrea <andrea.marcheggiani@aesys.tech>
2022-04-22 17:19:31 +0200

    Deleting Comments

--- library-online-demo/src/main/java/org/library/demo/dtos/UserLibraryDto.java
+++ library-online-demo/src/main/java/org/library/demo/dtos/UserLibraryDto.java
@@ -36,52 +36,5 @@ public class UserLibraryDto {
     public void setTitles(List<Title> titles) {
         this.titles = titles;
     }
-    //    private String name;
-//    private String surname;
-//    private Date birthday;
-//    private String taxCode;
-//    private AddressDto addressDto;
-//
-//    public UserLibraryDto() {
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getSurname() {
-//        return surname;
-//    }
-//
-//    public void setSurname(String surname) {
-//        this.surname = surname;
-//    }
-//
-//    public Date getBirthday() {
-//        return birthday;
-//    }
-//
-//    public void setBirthday(Date birthday) {
-//        this.birthday = birthday;
-//    }
-//
-//    public String getTaxCode() {
-//        return taxCode;
-//    }
-//
-//    public void setTaxCode(String taxCode) {
-//        this.taxCode = taxCode;
-//    }
-//
-//    public AddressDto getAddressDto() {
-//        return addressDto;
-//    }
-//
-//    public void setAddressDto(AddressDto addressDto) {
-//        this.addressDto = addressDto;
-//    }
+
 }
