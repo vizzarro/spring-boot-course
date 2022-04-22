e6acb127e9bca29f7be1efd6a303d0ff97539997
Andrea <andrea.marcheggiani@aesys.tech>
2022-04-22 16:50:53 +0200

    Add Dtos and new Abstract Class Dto

--- library-online-demo/src/main/java/org/library/demo/dtos/PersonDto.java
+++ library-online-demo/src/main/java/org/library/demo/dtos/PersonDto.java
@@ -3,74 +3,7 @@ package org.library.demo.dtos;
 
 import java.util.Date;
 
-public class PersonDto {
+public class PersonDto extends UserLibraryDto {
 
-    private String name;
-    private String surname;
-    private Date birthday;
-    private String taxCode;
-    private AddressDto addressDto;
-
-    public PersonDto(String name, String surname, Date birthday, String taxCode, AddressDto address) {
-        this.name = name;
-        this.surname = surname;
-        this.birthday = birthday;
-        this.taxCode = taxCode;
-        this.addressDto = address;
-    }
-
-    public PersonDto() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Date getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getTaxCode() {
-        return taxCode;
-    }
-
-    public void setTaxCode(String taxCode) {
-        this.taxCode = taxCode;
-    }
-
-    public AddressDto getAddressDto() {
-        return addressDto;
-    }
-
-    public void setAddressDto(AddressDto addressDto) {
-        this.addressDto = addressDto;
-    }
-
-    @Override
-    public String toString() {
-        return "PersonDto{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", birthday=" + birthday +
-                ", taxCode='" + taxCode + '\'' +
-                ", addressDto=" + addressDto +
-                '}';
-    }
 }
 
