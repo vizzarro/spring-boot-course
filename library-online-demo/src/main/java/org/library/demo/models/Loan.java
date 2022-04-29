8a957774e3bb414c559f1fe4334b7f0649c6b86d
Lucy <lucia.dalfonso@aesys.tech>
2022-04-29 15:17:19 +0200

    Mapping Loan class e create LoanId

--- library-online-demo/src/main/java/org/library/demo/models/Loan.java
+++ library-online-demo/src/main/java/org/library/demo/models/Loan.java
@@ -2,25 +2,32 @@ package org.library.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import jakarta.persistence.*;
 
 import java.util.Date;
-
+@Entity
+@Table( name = "loans")
 public class Loan {
+
+    @EmbeddedId
+    private LoanId loanid;
+
+    @ManyToOne
+    @MapsId("taxCode")
+    private UserLibrary userLibrary;
+
+    @ManyToOne
+    @MapsId("titleId")
+    private Title title;
 
     @JsonFormat(shape = Shape.STRING, pattern = "dd/MM/yyyy")
     private Date creationDate;
-    private String titleId;
-    private String taxCode;
+
 
     public Loan() {
         this.creationDate = new Date();
     }
 
-    public Loan(Date creationDate, String titleId, String taxCode) {
-        this.creationDate = creationDate;
-        this.titleId = titleId;
-        this.taxCode = taxCode;
-    }
 
     public Date getCreationDate() {
         return creationDate;
@@ -30,19 +37,4 @@ public class Loan {
         if (creationDate != null) this.creationDate = creationDate;
     }
 
-    public String getTitleId() {
-        return titleId;
-    }
-
-    public void setTitleId(String titleId) {
-        this.titleId = titleId;
-    }
-
-    public String getTaxCode() {
-        return taxCode;
-    }
-
-    public void setTaxCode(String taxCode) {
-        this.taxCode = taxCode;
-    }
 }
