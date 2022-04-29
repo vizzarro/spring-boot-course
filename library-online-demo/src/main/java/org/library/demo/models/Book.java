dca87b02f90d420e2d17ce8ee04572c3dfefe351
Lucy <lucia.dalfonso@aesys.tech>
2022-04-29 15:09:50 +0200

    Mapping Book class

--- library-online-demo/src/main/java/org/library/demo/models/Book.java
+++ library-online-demo/src/main/java/org/library/demo/models/Book.java
@@ -1,9 +1,21 @@
 package org.library.demo.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "title")
 public class Book implements Title {
+
+    @Id
+    @Column(name = "title_id")
+    private String titleId;
+
+    @Column(name = "name")
     private String name;
     private static int lendingTime = 30;
-    private String titleId;
 
     public Book() {
     }
