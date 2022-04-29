96c75ea0ce6954c9854b043358f0713fb350d811
Josè Giampaolo <josegiampaolo@gmail.com>
2022-04-29 16:47:57 +0200

    magazine

--- library-online-demo/src/main/java/org/library/demo/models/Magazine.java
+++ library-online-demo/src/main/java/org/library/demo/models/Magazine.java
@@ -1,9 +1,35 @@
 package org.library.demo.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "title")
 public class Magazine implements Title {
-    private String name;
-    private static int lendingTime = 10;
+
+    @Id
+    @Column(name = "title_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String titleId;
+
+    @Column(name = "name")
+    private String name;
+
+    private static int lendingTime = 10;
+
+    @OneToMany(mappedBy = "reservationId.titleId")
+//    @JoinTable(name = "reservation",
+//            joinColumns = @JoinColumn(name = "tax_code"),
+//            inverseJoinColumns = @JoinColumn(name = "title_id"))
+    private List<Reservation> reservations;
+
+    @OneToMany(mappedBy = "loanId.titleId")
+//    @JoinTable(name = "reservation",
+//            joinColumns = @JoinColumn(name = "tax_code"),
+//            inverseJoinColumns = @JoinColumn(name = "title_id"))
+    private List<Loan> loans;
 
     public Magazine() {
     }
@@ -29,5 +55,45 @@ public class Magazine implements Title {
     public void setTitleId(String titleId) {
         this.titleId = titleId;
     }
+
+    public List<Reservation> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Reservation> reservations) {
+        this.reservations = reservations;
+    }
+    public void addReservation(Reservation tempReservation) {
+
+        if (null == reservations) {
+            reservations = new ArrayList<>();
+        }
+
+        reservations.add(tempReservation);
+        tempReservation.setTitle(this);
+    }
+
+    public void addLoan(Loan tempLoan) {
+
+        if (null == loans) {
+            loans = new ArrayList<>();
+        }
+
+        loans.add(tempLoan);
+        tempLoan.setTitle(this);
+    }
+
+    public List<Loan> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(List<Loan> loans) {
+        this.loans = loans;
+    }
+
+    public Magazine(List<Loan> loans) {
+        this.loans = loans;
+    }
+
 
 }
