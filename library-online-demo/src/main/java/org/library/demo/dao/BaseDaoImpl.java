7477ac02740534d0df075c582703207d87b4b3bf
alessio <alessio.cipollone@aesys.tech>
2022-04-29 17:58:09 +0200

    add config xml and session method in BaseDaoImpl

--- library-online-demo/src/main/java/org/library/demo/dao/BaseDaoImpl.java
+++ library-online-demo/src/main/java/org/library/demo/dao/BaseDaoImpl.java
@@ -4,6 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.library.demo.models.*;
+
 public abstract class BaseDaoImpl<T, K> implements BaseDao<T, K> {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/AcademyJava";
@@ -32,5 +37,18 @@ public abstract class BaseDaoImpl<T, K> implements BaseDao<T, K> {
                 connection.close();
             } catch (SQLException e) {/*Do Nothing*/}
         }
+    }
+
+    public static Session getSession(){
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Book.class)
+                .addAnnotatedClass(Loan.class)
+                .addAnnotatedClass(Magazine.class)
+                .addAnnotatedClass(Reservation.class)
+                .addAnnotatedClass(UserLibrary.class)
+                .buildSessionFactory();
+
+        return factory.getCurrentSession();
     }
 }
