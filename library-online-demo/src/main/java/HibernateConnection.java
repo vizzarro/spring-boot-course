de4f619629758f82c59e9648265ff9f26239526f
Lucia D'Alfonso <lucia.dalfonso@aesys.tech>
2022-05-01 14:55:10 +0200

    Add connection

---
+++ library-online-demo/src/main/java/HibernateConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class HibernateConnection {
+
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/AcademyJava?useSSL=false&serverTimezone=UTC";
+        String user = "postgres";
+        String password = "password";
+
+        try {
+            Connection myConn = DriverManager.getConnection(jdbcUrl,user,password);
+            System.out.println("connessione al db riuscita");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
