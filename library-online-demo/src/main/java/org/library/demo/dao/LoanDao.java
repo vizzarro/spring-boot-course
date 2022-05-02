23c3beab431ca279d82930f1046bf0ebf7c6519a
Giammarco <giammarco.cataldi@aesys.tech>
2022-05-02 11:45:45 +0200

    param K DaoInterface

--- library-online-demo/src/main/java/org/library/demo/dao/LoanDao.java
+++ library-online-demo/src/main/java/org/library/demo/dao/LoanDao.java
@@ -2,5 +2,6 @@ package org.library.demo.dao;
 
 import org.library.demo.models.Loan;
 
-public interface LoanDao extends BaseDao<Loan, Loan> {
+public interface LoanDao extends BaseDao<Loan, String> {
+
 }
--- library-online-demo/src/main/java/org/library/demo/dao/ReservationDao.java
+++ library-online-demo/src/main/java/org/library/demo/dao/ReservationDao.java
@@ -2,5 +2,5 @@ package org.library.demo.dao;
 
 import org.library.demo.models.Reservation;
 
-public interface ReservationDao extends BaseDao<Reservation, Reservation> {
+public interface ReservationDao extends BaseDao<Reservation, String> {
 }
